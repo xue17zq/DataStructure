0da939545fb338a8d944aae9bd32247fdad272f2
Your Name <you@example.com>
2026-09-13 11:02:29 +0800

    我修改了第三次作业

--- 2025010013李苹/Lab3/seqlist.c
+++ 2025010013李苹/Lab3/seqlist.c
@@ -1,13 +1,12 @@
-//
-// Created by 联想电脑 on 2026/9/9.
-//
 #include <stdio.h>
 #define MAX_SIZE 100
+
 typedef struct {
     int data[MAX_SIZE];
     int size;
 } SeqList;
 
+//练习1：遍历打印
 void printList(const SeqList *list)
 {
     for (int i = 0; i < list->size; i++)
@@ -19,12 +18,15 @@ void printList(const SeqList *list)
     printf("\n");
 }
 
+//练习2：插入
 int insertAt(SeqList *list, int index, int value)
 {
+    //满了 或者下标非法，返回0
     if (list->size == MAX_SIZE || index < 0 || index > list->size)
     {
         return 0;
     }
+    //从后往前移动元素
     for(int i = list->size; i > index; i--)
     {
         list->data[i] = list->data[i-1];
@@ -34,13 +36,16 @@ int insertAt(SeqList *list, int index, int value)
     return 1;
 }
 
+//练习3：删除
 int removeAt(SeqList *list, int index, int *removed)
 {
+    //下标非法返回0
     if(index < 0 || index >= list->size)
     {
         return 0;
     }
     *removed = list->data[index];
+    //后面元素前移
     for(int i = index; i < list->size - 1; i++)
     {
         list->data[i] = list->data[i+1];
@@ -52,6 +57,7 @@ int removeAt(SeqList *list, int index, int *removed)
 int main()
 {
     SeqList list;
+
     list.size = 4;
     list.data[0] = 10;
     list.data[1] = 20;
